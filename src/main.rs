206b0da9f36fa44f40d89c03c22ff5195290ab76
shrikant23codes <jiturishrikant@gmail.com>
2025-12-25 01:03:41 +0530

    editor + preview side by side

--- src/main.rs
+++ src/main.rs
@@ -32,20 +32,33 @@ impl Default for MyApp {
 impl eframe::App for MyApp {
 
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+
+        egui::SidePanel::left("editor_panel")
+            .exact_width(ctx.screen_rect().width() / 2.0)
+            .show(ctx, |ui| {
+                ui.heading("Editor");
+                ui.separator();
+
+                egui::TextEdit::multiline(&mut self.user_text)
+                    .desired_width(f32::INFINITY)  // Fill available width
+                    .desired_rows(25)
+                    .show(ui);
+
+                ui.label(format!("Characters: {}", self.user_text.len()));
+            });
         
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Namaskar, world!!");
-
+            ui.heading("Preview");
             ui.separator();
 
-            egui::TextEdit::multiline(&mut self.user_text)
-                .desired_width(f32::INFINITY)
-                .desired_rows(20)
-                .show(ui);
-
-            ui.separator();
-
-            ui.label(format!("Characters: {}", self.user_text.len()));
+            egui::ScrollArea::vertical()
+                .show(ui, |ui| {
+                    ui.label(
+                        egui::RichText::new(&self.user_text)
+                            .size(14.0)
+                    );
+                });
         });
+
     }
 }
